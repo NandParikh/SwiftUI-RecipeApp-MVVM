823a3c5db70c221979a7305097e8e3efdcab4cec
NandParikh <nand.parikh.apple@gmail.com>
2025-11-10 10:57:30 +0530

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by                     Nand Parikh on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
